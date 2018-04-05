28e3ba4a9da369970c04bafddd64460d9507e244
Josiah <spacetagman@gmail.com>
2018-04-04 23:35:42 -0500

    Renaming pseudocode renames definition. And the definition block is grey

--- appengine/turtle/js/blocks.js
+++ appengine/turtle/js/blocks.js
@@ -416,24 +416,6 @@ Blockly.Blocks['turtle_repeat_internal'] = {
 Blockly.JavaScript['turtle_repeat_internal'] =
     Blockly.JavaScript['controls_repeat'];
 
-Blockly.Blocks['stub'] = {
-  init: function() {
-    this.appendValueInput("unused_function_return_value")
-        .setCheck(null);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour("#8e8e8e");
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-Blockly.JavaScript['stub'] = function(block) {
-  var value_unused_function_return_value = Blockly.JavaScript.valueToCode(block, 'unused_function_return_value', Blockly.JavaScript.ORDER_COMMA);
-  var code = value_unused_function_return_value + ';\n';
-  return code;
-};
-
 Blockly.Blocks['procedures_pseudo_callreturn'] = {
   /**
    * Renamable block for calling a procedure with a return value.
@@ -476,4 +458,175 @@ Blockly.JavaScript['procedures_pseudo_callreturn'] = function(block) {
   }
   var code = funcName + '(' + args.join(', ') + ')';
   return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+Blockly.Procedures.renameForCall = function(name) {
+
+  // Strip leading and trailing whitespace.  Beyond this, all names are legal.
+  name = name.replace(/^[\s\xa0]+|[\s\xa0]+$/g, '');
+
+  // Ensure two identically-named procedures don't exist.
+  var legalName = Blockly.Procedures.findLegalNameForCall(name, this.sourceBlock_);
+  var oldName = this.text_;
+  if (oldName != name && oldName != legalName) {
+    // Rename any callers.
+    var blocks = this.sourceBlock_.workspace.getAllBlocks();
+    for (var i = 0; i < blocks.length; i++) {
+      if (blocks[i].renameProcedureDef) {
+        console.log("ASDF")
+        // Blockly.Events.disable();
+        blocks[i].renameProcedureDef(oldName, legalName);
+        // Blockly.Events.enable();
+      }
+    }
+  }
+  console.log("rename", name, legalName)
+  return legalName;
+};
+
+/**
+ * Ensure two identically-named procedures don't exist.
+ * @param {string} name Proposed procedure name.
+ * @param {!Blockly.Block} block Block to disambiguate.
+ * @return {string} Non-colliding name.
+ */
+Blockly.Procedures.findLegalNameForCall = function(name, block) {
+  return name; // TODO: Why is this looping otherwise?
+  if (block.isInFlyout) {
+    // Flyouts can have multiple procedures called 'do something'.
+    return name;
+  }
+  while (!Blockly.Procedures.isLegalNameForCall_(name, block.workspace, block)) {
+    // Collision with another procedure.
+    var r = name.match(/^(.*?)(\d+)$/);
+    if (!r) {
+      name += '2';
+    } else {
+      name = r[1] + (parseInt(r[2], 10) + 1);
+    }
+  }
+  return name;
+};
+
+/**
+ * Does this procedure have a legal name?  Illegal names include names of
+ * procedures already defined.
+ * @param {string} name The questionable name.
+ * @param {!Blockly.Workspace} workspace The workspace to scan for collisions.
+ * @param {Blockly.Block=} opt_exclude Optional block to exclude from
+ *     comparisons (one doesn't want to collide with oneself).
+ * @return {boolean} True if the name is legal.
+ * @private
+ */
+Blockly.Procedures.isLegalNameForCall_ = function(name, workspace, opt_exclude) {
+  return !Blockly.Procedures.isNameUsedForCall(name, workspace, opt_exclude);
+};
+
+/**
+ * Return if the given name is already a procedure name.
+ * @param {string} name The questionable name.
+ * @param {!Blockly.Workspace} workspace The workspace to scan for collisions.
+ * @param {Blockly.Block=} opt_exclude Optional block to exclude from
+ *     comparisons (one doesn't want to collide with oneself).
+ * @return {boolean} True if the name is used, otherwise return false.
+ */
+Blockly.Procedures.isNameUsedForCall = function(name, workspace, opt_exclude) {
+  var blocks = workspace.getAllBlocks();
+  // Iterate through every block and check the name.
+  for (var i = 0; i < blocks.length; i++) {
+    if (blocks[i] == opt_exclude) {
+      continue;
+    }
+    if (blocks[i].getProcedureDef) {
+      var procName = blocks[i].getProcedureDef();
+      if (Blockly.Names.equals(procName[0], name)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+Blockly.Blocks['procedures_defreturn'] = {
+  /**
+   * Block for defining a procedure with a return value.
+   * @this Blockly.Block
+   */
+  init: function() {
+    var nameField = new Blockly.FieldTextInput('');
+    nameField.setSpellcheck(false);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.PROCEDURES_DEFRETURN_TITLE)
+        .appendField(nameField, 'NAME')
+        .appendField('', 'PARAMS');
+    this.appendValueInput('RETURN')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(Blockly.Msg.PROCEDURES_DEFRETURN_RETURN);
+    this.setMutator(new Blockly.Mutator(['procedures_mutatorarg']));
+    if ((this.workspace.options.comments ||
+         (this.workspace.options.parentWorkspace &&
+          this.workspace.options.parentWorkspace.options.comments)) &&
+        Blockly.Msg.PROCEDURES_DEFRETURN_COMMENT) {
+      this.setCommentText(Blockly.Msg.PROCEDURES_DEFRETURN_COMMENT);
+    }
+    this.setColour("#8e8e8e");
+    this.setTooltip(Blockly.Msg.PROCEDURES_DEFRETURN_TOOLTIP);
+    this.setHelpUrl(Blockly.Msg.PROCEDURES_DEFRETURN_HELPURL);
+    this.arguments_ = [];
+    this.setStatements_(true);
+    this.statementConnection_ = null;
+  },
+  setStatements_: Blockly.Blocks['procedures_defnoreturn'].setStatements_,
+  updateParams_: Blockly.Blocks['procedures_defnoreturn'].updateParams_,
+  mutationToDom: Blockly.Blocks['procedures_defnoreturn'].mutationToDom,
+  domToMutation: Blockly.Blocks['procedures_defnoreturn'].domToMutation,
+  decompose: Blockly.Blocks['procedures_defnoreturn'].decompose,
+  compose: Blockly.Blocks['procedures_defnoreturn'].compose,
+  /**
+   * Return the signature of this procedure definition.
+   * @return {!Array} Tuple containing three elements:
+   *     - the name of the defined procedure,
+   *     - a list of all its arguments,
+   *     - that it DOES have a return value.
+   * @this Blockly.Block
+   */
+  getProcedureDef: function() {
+    return [this.getFieldValue('NAME'), this.arguments_, true];
+  },
+  getVars: Blockly.Blocks['procedures_defnoreturn'].getVars,
+  renameVar: Blockly.Blocks['procedures_defnoreturn'].renameVar,
+  customContextMenu: Blockly.Blocks['procedures_defnoreturn'].customContextMenu,
+  callType_: 'procedures_callreturn'
+};
+
+Blockly.Blocks['procedures_defreturn'].renameProcedureDef = function(oldName, newName) {
+  console.log("hey")
+  console.log("oldName", oldName)
+  console.log("legalname", newName)
+  console.log(this.getProcedureDef()[0])
+  if (Blockly.Names.equals(oldName, this.getProcedureDef()[0])) {
+    this.setFieldValue(newName, 'NAME');
+    this.setTooltip(
+        (this.outputConnection ? Blockly.Msg.PROCEDURES_CALLRETURN_TOOLTIP :
+         Blockly.Msg.PROCEDURES_CALLNORETURN_TOOLTIP)
+        .replace('%1', newName));
+  }
+}
+
+Blockly.Blocks['stub'] = {
+  init: function() {
+    this.appendValueInput("unused_function_return_value")
+        .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour("#8e8e8e");
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['stub'] = function(block) {
+  var value_unused_function_return_value = Blockly.JavaScript.valueToCode(block, 'unused_function_return_value', Blockly.JavaScript.ORDER_COMMA);
+  var code = value_unused_function_return_value + ';\n';
+  return code;
 };
